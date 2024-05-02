955ed54fbd7edc8fc2e78d7a0f553402fe6af8b5
emmaspitzpolestar <emma.spitz@polestar.com>
2024-05-02 13:10:58 +0200

    add prettier and husky

---
+++ src/app/om/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Om</h1>
+    </div>
+  )
+}
---
+++ src/app/produkter/page.tsx
@@ -0,0 +1,7 @@
+export const page = () => {
+  return (
+    <div>
+      <h1>Produkter</h1>
+    </div>
+  )
+}
